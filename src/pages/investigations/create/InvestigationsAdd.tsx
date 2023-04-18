935b7451fcb5f6ca1ceebe0e5603d33e6dc706ef
chiucchi <gchiucchi01@gmail.com>
2023-04-18 14:57:59 -0300

    added involveds logic and form

--- src/pages/investigations/create/InvestigationsAdd.tsx
+++ src/pages/investigations/create/InvestigationsAdd.tsx
@@ -6,18 +6,28 @@ import {
   DatePicker,
   DatePickerProps,
   Divider,
+  Form,
   Input,
   Row,
+  Select,
   Space,
   Typography,
 } from "antd";
 import PageContainer from "../../../components/container/Container";
 import { useState } from "react";
 import { CheckboxChangeEvent } from "antd/es/checkbox";
-import { DeleteOutlined, PlusOutlined, SendOutlined } from "@ant-design/icons";
+import {
+  DeleteOutlined,
+  MinusCircleOutlined,
+  PlusCircleOutlined,
+  PlusOutlined,
+  SendOutlined,
+} from "@ant-design/icons";
 import JSZip from "jszip";
+const { Option } = Select;
 
 const InvestigationsAdd = () => {
+  const [form] = Form.useForm();
   const [checked, setChecked] = useState(false);
   const [files, setFiles] = useState<File[]>([]);
   const [filenames, setFilenames] = useState<string[]>([]);
@@ -35,6 +45,7 @@ const InvestigationsAdd = () => {
 
   const onChangeCheck = (e: CheckboxChangeEvent) => {
     console.log("checked = ", e.target.checked);
+    form.setFieldsValue({ involveds: "" });
     setChecked(e.target.checked);
   };
 
@@ -54,70 +65,142 @@ const InvestigationsAdd = () => {
     console.log(files);
   };
 
+  const onFinish = (values: any) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  const sights = {
+    Beijing: ["Tiananmen", "Great Wall"],
+    Shanghai: ["Oriental Pearl", "The Bund"],
+  };
+
+  const handleChange = () => {
+    form.setFieldsValue({ sights: [] });
+  };
+
   return (
     <PageContainer>
       <Typography.Title>Adicionar dados</Typography.Title>
-      <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, 24]} align="middle">
-        <Col span={12}>
-          <Input showCount maxLength={40} onChange={onChange} />
-        </Col>
-        <Col span={12}>
-          <Input disabled value="John Doe" />
-        </Col>
-        <Col span={6}>
-          <DatePicker onChange={onChangeDate} />
-        </Col>
-        <Col span={12}>
-          <Checkbox checked={checked} onChange={onChangeCheck}>
-            Adicionar envolvidos conhecidos na investigação
-          </Checkbox>
-        </Col>
-      </Row>
-      {checked && (
-        <>
-          <Divider />
-          <Typography.Title level={4}>Envolvidos</Typography.Title>
-          <Row gutter={16}>
-            <Col span={12}>
-              <Input onChange={onChange} />
-            </Col>
-            <Space>
-              <Button>
-                <PlusOutlined />
-              </Button>
-              <Button>
-                <DeleteOutlined />
-              </Button>
-            </Space>
-          </Row>
-        </>
-      )}
-      <Divider />
-      <Space direction="vertical" size="large" style={{ display: "flex" }}>
-        <Typography.Title level={4}>Inserir arquivos</Typography.Title>
-        <input type="file" onChange={handleFileSelect} />
-        {filenames.length > 0 && (
-          <Card title={mainFile} style={{ width: "50%" }}>
-            <ul>
-              {filenames.map((filename) => {
-                return <li key={filename}>{filename}</li>;
-              })}
-            </ul>
-          </Card>
-        )}
-      </Space>
-      <div
-        style={{
-          position: "absolute",
-          bottom: "32px",
-          right: "32px",
-        }}
+      <Form
+        form={form}
+        name="add-investigation"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <Button type="primary">
-          <SendOutlined />
-          Enviar dados
-        </Button>
-      </div>
+        <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, 24]} align="top">
+          <Col span={12}>
+            <Form.Item
+              label="Nome"
+              name="name"
+              rules={[
+                { required: true, message: "Por favor adicione um nome" },
+              ]}
+            >
+              <Input showCount maxLength={40} onChange={onChange} />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Input value="John Doe" disabled />
+          </Col>
+          <Col span={8}>
+            <Form.Item
+              label="Data de início"
+              name="date"
+              rules={[
+                {
+                  required: true,
+                  message: "Por favor adicione uma data próxima",
+                },
+              ]}
+            >
+              <DatePicker onChange={onChangeDate} />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Checkbox checked={checked} onChange={onChangeCheck}>
+              Adicionar envolvidos conhecidos na investigação
+            </Checkbox>
+          </Col>
+        </Row>
+        {checked && (
+          <>
+            <Divider />
+            <Typography.Title level={4}>Envolvidos</Typography.Title>
+            <Form.List name="involveds">
+              {(fields, { add, remove }) => {
+                return (
+                  <>
+                    {fields.map((field) => (
+                      <Row gutter={16} key={field.key} align="middle">
+                        <Col span={12}>
+                          <Form.Item
+                            {...field}
+                            name={[field.name, "involved"]}
+                            rules={[
+                              {
+                                required: true,
+                                message:
+                                  "Favor adicionar um nome, ou remover o envolvido",
+                              },
+                            ]}
+                          >
+                            <Input />
+                          </Form.Item>
+                        </Col>
+                        <Col span={6} style={{ marginBottom: "24px" }}>
+                          <MinusCircleOutlined
+                            onClick={() => remove(field.name)}
+                          />
+                        </Col>
+                      </Row>
+                    ))}
+                    <Button
+                      type="dashed"
+                      onClick={() => add()}
+                      block
+                      icon={<PlusOutlined />}
+                    >
+                      Adicionar envolvido
+                    </Button>
+                  </>
+                );
+              }}
+            </Form.List>
+          </>
+        )}
+        <Divider />
+        <Space direction="vertical" size="large" style={{ display: "flex" }}>
+          <Typography.Title level={4}>Inserir arquivos</Typography.Title>
+          <input type="file" onChange={handleFileSelect} />
+          {filenames.length > 0 && (
+            <Card title={mainFile} style={{ width: "50%" }}>
+              <ul>
+                {filenames.map((filename) => {
+                  return <li key={filename}>{filename}</li>;
+                })}
+              </ul>
+            </Card>
+          )}
+        </Space>
+        <div
+          style={{
+            position: "absolute",
+            bottom: "32px",
+            right: "32px",
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            <SendOutlined />
+            Enviar dados
+          </Button>
+        </div>
+      </Form>
     </PageContainer>
   );
 };
