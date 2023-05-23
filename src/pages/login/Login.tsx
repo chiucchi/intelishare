4ea7a53c3ce2d244c95215b1dfc39bc5a97e9853
chiucchi <gchiucchi01@gmail.com>
2023-05-23 16:00:06 -0300

    add edit logic to edit profile

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -8,11 +8,20 @@ function Login() {
   const navigate = useNavigate();
 
   const onFinish = async (values: any) => {
-    await api.post("/login", values).then((res) => {
-      Cookies.remove("token"); // para renovar o token caso esteja expirado // fazer isso de outro jeito com o refresh token
-      Cookies.set("token", res.data.token);
-      navigate("/home");
-    });
+    await api
+      .post("/login", values)
+      .then((res) => {
+        Cookies.remove("token"); // para renovar o token caso esteja expirado // fazer isso de outro jeito com o refresh token
+        Cookies.set("token", res.data.token);
+        navigate("/home");
+      })
+      .catch((err) => {
+        notification.open({
+          type: "error",
+          message: "Ocorreu um erro ao logar",
+          description: err.response.data.message,
+        });
+      });
   };
 
   const onFinishFailed = (errorInfo: any) => {
--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -4,8 +4,11 @@ import {
   Col,
   DatePicker,
   DatePickerProps,
+  Descriptions,
   Divider,
+  Form,
   Input,
+  Popconfirm,
   Row,
   Select,
   Space,
@@ -13,10 +16,15 @@ import {
   Typography,
 } from "antd";
 import PageContainer from "../../components/container/Container";
-import { UserOutlined } from "@ant-design/icons";
+import { QuestionCircleOutlined, UserOutlined } from "@ant-design/icons";
 import { ufData } from "../../mocks/uf-mock";
+import { extractUser } from "../../helpers/getUser";
+import { useState } from "react";
 
 const Profile = () => {
+  const [edit, setEdit] = useState(false);
+  const user = extractUser();
+
   const onChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {};
@@ -28,67 +36,124 @@ const Profile = () => {
   return (
     <PageContainer>
       <Typography.Title>Meu perfil</Typography.Title>
-      <Row align="middle" style={{ margin: "48px 0 48px" }}>
-        <Col span={4}>
-          <Space
-            direction="vertical"
-            size="middle"
-            style={{ cursor: "pointer" }}
+      <Form name="basic" initialValues={{ remember: true }} autoComplete="off">
+        <Row align="middle" style={{ margin: "48px 0 48px" }}>
+          <Col span={4}>
+            <Space
+              direction="vertical"
+              size="middle"
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                // change profile pic
+              }}
+            >
+              <Avatar
+                size={128}
+                icon={<UserOutlined />}
+                style={{ backgroundColor: "#D9D9D9" }}
+              />
+            </Space>
+          </Col>
+          <Col span={18}>
+            <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, 24]}>
+              <Col span={12}>
+                <Form.Item label="Nome" name="name">
+                  {!edit ? (
+                    <>{user.name}</>
+                  ) : (
+                    <Input defaultValue={user.name} onChange={onChange} />
+                  )}
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item label="Email" name="email">
+                  {!edit ? (
+                    <Descriptions.Item label="Email">
+                      {user.email}
+                    </Descriptions.Item>
+                  ) : (
+                    <Input defaultValue={user.email} onChange={onChange} />
+                  )}
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item label="Senha" name="password">
+                  {!edit ? (
+                    <Descriptions.Item label="Senha">
+                      ************
+                    </Descriptions.Item>
+                  ) : (
+                    <Input
+                      type="password"
+                      defaultValue="************"
+                      onChange={onChange}
+                    />
+                  )}
+                </Form.Item>
+              </Col>
+              <Col span={6}>
+                <Form.Item label="Data de Nascimento" name="birthDate">
+                  <DatePicker
+                    onChange={onChangeDate}
+                    format="DD/MM/YYYY"
+                    disabled={!edit}
+                  />
+                </Form.Item>
+              </Col>
+              <Col span={6}>
+                <Form.Item label="UF" name="uf">
+                  <Select
+                    defaultValue="MG"
+                    style={{ width: 120 }}
+                    onChange={handleChange}
+                    options={ufData}
+                    disabled={!edit}
+                  />
+                </Form.Item>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        <Space
+          style={{
+            position: "absolute",
+            bottom: "32px",
+            right: "32px",
+          }}
+          size="large"
+        >
+          {!edit ? (
+            <Popconfirm
+              title="Deletar conta"
+              description="Você tem certeza que deseja excluir a sua conta e todas as suas investigações?"
+              okText="Sim"
+              cancelText="Não"
+              icon={<QuestionCircleOutlined style={{ color: "red" }} />}
+              onConfirm={() => {
+                // delete account
+              }}
+            >
+              <Button>Deletar conta</Button>
+            </Popconfirm>
+          ) : (
+            <Button type="dashed" onClick={() => setEdit(false)}>
+              Cancelar
+            </Button>
+          )}
+          <Button
+            type="primary"
             onClick={() => {
-              // change profile pic
+              if (edit) {
+                // save changes
+              } else {
+                setEdit(true);
+              }
             }}
           >
-            <Avatar
-              size={128}
-              icon={<UserOutlined />}
-              style={{ backgroundColor: "#D9D9D9" }}
-            />
-          </Space>
-        </Col>
-        <Col span={18}>
-          <Row gutter={[{ xs: 8, sm: 16, md: 24, lg: 32 }, 24]}>
-            <Col span={12}>
-              <Input value="John Doe" />
-            </Col>
-            <Col span={12}>
-              <Input value="John@Doe.com" />
-            </Col>
-            <Col span={12}>
-              <Input.Password value="38291732189" onChange={onChange} />
-            </Col>
-            <Col span={6}>
-              <DatePicker onChange={onChangeDate} format="DD/MM/YYYY" />
-            </Col>
-            <Col span={6}>
-              <Select
-                defaultValue="MG"
-                style={{ width: 120 }}
-                onChange={handleChange}
-                options={ufData}
-              />
-            </Col>
-          </Row>
-        </Col>
-      </Row>
-      <Space
-        style={{
-          position: "absolute",
-          bottom: "32px",
-          right: "32px",
-        }}
-        size="large"
-      >
-        <Button>Deletar conta</Button>
-        <Button
-          type="primary"
-          onClick={() => {
-            console.log("edit account button clicked");
-            // activate inputs, or transform description to inputs
-          }}
-        >
-          Editar
-        </Button>
-      </Space>
+            {edit ? "Salvar" : "Editar"}
+          </Button>
+        </Space>
+      </Form>
     </PageContainer>
   );
 };
